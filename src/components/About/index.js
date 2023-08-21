a6907e456ea2606fa401cdedba9d5d8c61d2fae4
lekhya11 <lekhya.kondaveeti@gmail.com>
2023-08-21 16:29:14 +0000

    navbar with context

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const lightImage =
+  'https://assets.ccbp.in/frontend/react-js/about-light-img.png'
+const darkImage = 'https://assets.ccbp.in/frontend/react-js/about-dark-img.png'
+
+const About = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-light-img.png"
+      alt="about"
+      className="image"
+    />
+    <h1> About </h1>
+  </div>
+)
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const lightImage = 'https://assets.ccbp.in/frontend/react-js/home-light-img.png'
+
+const darkImage = 'https://assets.ccbp.in/frontend/react-js/home-dark-img.png'
+
+const Home = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-light-img.png"
+      alt="about"
+      className="image"
+    />
+    <h1> Home </h1>
+  </div>
+)
+
+export default Home
